3aef4cf2f32ec674bd832ba489699e0f56f0256f
JcB <jcbagneris@gmail.com>
2009-08-26 18:11:02 +0200

    Création du setting CONTENTS_PREFIX

--- settings.py
+++ settings.py
@@ -60,6 +60,7 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/static/media/'
+CONTENTS_PREFIX = '/static/contents/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '%^)v@no(0iz_m5sx*mn#k2%#emm3m0117f2k4ltd+gg=ugz3ws'
