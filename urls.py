c6e71361111d56d4f648c562f031a5bf649ca2ea
JcB <jcbagneris@gmail.com>
2009-09-03 18:58:57 +0200

    Mise en place du reset du password

--- urls.py
+++ urls.py
@@ -8,18 +8,31 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^$', 'dashboard.views.dashboard'),
-    (r'^staff/', include(admin.site.urls)),
-    (r'^login/$', 'email_auth.views.login'),
-    (r'^logout/$', 'email_auth.views.logout'),
-    (r'^dashboard/$', 'dashboard.views.dashboard'),
-    (r'^profile/$', 'coaching.views.profile'),
-    (r'^learning/', include('learning.urls')),
+# Base
+urlpatterns = patterns('email_auth.views',
+    (r'^login/$', 'login'),
+    (r'^logout/$', 'logout'),
 )
 
+# Lost password
+urlpatterns += patterns('django.contrib.auth.views',
+    (r'^lostpw/$', 'password_reset'),
+    (r'^lostpw/done/$', 'password_reset_done'),
+    (r'^lostpw/confirm/(?P<uidb36>.*)/(?P<token>.*)$', 'password_reset_confirm'),
+    (r'^lostpw/complete/$', 'password_reset_complete'),
+)
+
+# Applications
+urlpatterns += patterns('',
+    (r'^$', 'dashboard.views.dashboard'),
+    (r'^learning/', include('learning.urls')),
+    (r'^staff/', include(admin.site.urls)),
+    (r'^dashboard/$', 'dashboard.views.dashboard'),
+    (r'^profile/$', 'coaching.views.profile'),
+)
+
+# Développement
 if settings.SITE_ID==1:
-    # dev server
     contents_root = os.path.join(settings.PROJECT_PATH, settings.CONTENTS_PREFIX)
     urlpatterns += patterns('',
     (r'^static/contents/(?P<path>.*)$', 'django.views.static.serve',
