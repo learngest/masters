2fb687530073b3781b174a26eab57e02fff091b3
JcB <jcbagneris@gmail.com>
2009-11-06 13:30:12 +0100

    Correction MEDIA_ROOT et MEDIA_URL

--- settings.py
+++ settings.py
@@ -45,12 +45,12 @@ USE_I18N = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True                                                                            
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_PATH, 'uploads'),
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'static/uploads')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/uploads/'
+MEDIA_URL = '/static/uploads/'
 
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
