0c3a09a3748d4dcc9cedacb3333fb259f4144603
JcB <jcbagneris@gmail.com>
2009-11-07 14:36:23 +0100

    Ajout du daily report

---
+++ scripts/daily_report.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import os
+import sys
+import datetime
+
+sys.path.append(os.path.dirname(os.path.abspath('%s/../..' % __file__)))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'masters.settings'
+from django.conf import settings
+
+from django.template.loader import render_to_string
+
+dummy = settings.INSTALLED_APPS
+
+from coaching.models import Groupe
+from learning.models import ModuleCours
+
+liste = []
+
+for groupe in Groupe.objects.order_by('client','nom',):
+    groupe.nb_cours = groupe.cours.count()
+    groupe.nb_modules = 0
+    for c in groupe.cours.all():
+        groupe.nb_modules += ModuleCours.objects.filter(cours=c).count()
+    groupe.nb_logins = groupe.utilisateur_set.count()
+    auj = datetime.date.today()
+    hier = auj - datetime.timedelta(1)
+    groupe.hier = groupe.utilisateur_set.filter(
+            last_login__gte=hier,last_login__lt=auj).count()
+    if len(groupe.client.nom) > 19:
+        groupe.clientname = groupe.client.nom[:15]+"..."
+    else:
+        groupe.clientname = groupe.client.nom
+    if len(groupe.nom) > 19:
+        groupe.nom = groupe.nom[:15]+"..."
+    liste.append(groupe)
+
+report = render_to_string('report.txt', {'liste_groupes': liste, 'hier': hier,})
+print report
