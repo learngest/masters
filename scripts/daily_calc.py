53c5e79c145c697d6c737a473d043ba9f930682f
JcB <jcbagneris@gmail.com>
2009-11-10 15:03:38 +0100

    Ajout script recalcul quotidien

---
+++ scripts/daily_calc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath('%s/../..' % __file__)))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'masters.settings'
+from django.conf import settings
+
+dummy = settings.INSTALLED_APPS
+
+from coaching.models import Utilisateur
+from coaching.controllers import UserState
+
+for utilisateur in Utilisateur.objects.all():
+    us = UserState(utilisateur)
+    us.nb_cours_valides(recalcul=True)
+    us.nb_travaux_rendus(recalcul=True)
+    us.cours_courant(recalcul=True)
+    us.nb_modules_in_current(recalcul=True)
+    us.nb_modules_valides_in_current(recalcul=True)
+
+
